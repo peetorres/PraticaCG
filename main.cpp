780180b2effd3f4e84815a742be0b2b973563dfc
Pedro Gabriel <pee.torres@gmail.com>
2018-05-23 20:35:32 -0300

    Prática de CG Raster 1,2 e 3
    Faltou o exercício 4, não sei fazer.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@
 using namespace std ;
 
 /*Variaveis Globais*/
-int COLUNAS = 200.0;
-int LINHAS = 200.0;
-int x0,y0,x1,y1;
-int x2,y2,x3,y3;
 int matRaster[200][200];
-
 
 /*Menu de Funcoes*/
 void unit(int, int, float, float, float, float);
@@ -20,6 +15,8 @@ void line(int, int, int, int);
 void setPixel(int, int);
 void preenchimentoAlastramento();
 void drawPolygonLine();
+void drawLine();
+void drawBaleia();
 
 void preenchimentoAlastramento(int x, int y){
     if(matRaster[x-1][y]!=1){
@@ -48,21 +45,33 @@ void preenchimentoAlastramento(int x, int y){
 }
 
 void drawBaleia(){
-    x0=50; y0=50;x1=50;y1=75;x2=75;y2=90;x3=130;y3=50;
-    int x4=130; int y4=70;int x5=100;int y5=50;
+    int x0=50, y0=50, x1=50, y1=75, x2=75, y2=90;
+    int x3=130, y3=50, x4=130, y4=70, x5=100, y5=50;
     line(x0,y0,x1,y1);
     line(x1,y1,x2,y2);
     line(x2,y2,x3,y3);
     line(x3,y3,x4,y4);
     line(x4,y4,x5,y5);
     line(x5,y5,x0,y0);
+
+    preenchimentoAlastramento(60,60);
+    preenchimentoAlastramento(125,65);
 }
 
 void drawPolygonLine(){
+    int x0 = 50, y0 = 50, x1 = 50, y1 = 150;
+    int x2 = 150, y2 = 150, x3 = 150, y3 = 50;
     line(x0,y0,x1,y1);
     line(x1,y1,x2,y2);
     line(x2,y2,x3,y3);
     line(x3,y3,x0,y0);
+    preenchimentoAlastramento(60,60);
+}
+
+void drawLine(){
+    int x0 = 1, y0 = 1;
+    int x1 = 199, y1 = 199;
+    line(x0,y0,x1,y1);
 }
 
 void setPixel(int x, int y){
@@ -105,11 +114,9 @@ void unit(int x, int y,float r, float g, float b, float alpha){
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
-    //line(x0, y0, x1, y1);
-    drawBaleia();
-    //drawPolygonLine();
-    preenchimentoAlastramento(60,60);
-    preenchimentoAlastramento(125,65);
+    //drawLine(); // Descomente essa para mostrar o resultado do exercicio 1
+    //drawPolygonLine(); // Descomente essa linha para mostrar o resultado do exercicio 2
+    drawBaleia(); // Descomente essa linha para mostrar o resultado do exercicio 3
 }
 
 void init(){
@@ -118,7 +125,7 @@ void init(){
     //define o sistema de visualizacao de projecao
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho (0.0 , COLUNAS , 0.0 , LINHAS , -1.0, 200);
+    glOrtho (0.0 , 200 , 0.0 , 200 , 0, 200);
     glMatrixMode(GL_MODELVIEW);
     for(int i=0; i<200; i++){
         for(int j=0; j<200 ;j++){
@@ -134,13 +141,6 @@ int main(int argc, char** argv){
     glutInitWindowPosition(150,150);
 
     glutCreateWindow("Pratica Rasterizacao");
-
-    cout << "Entre com as coordenadas x0, y0, x1, y1, respectivamente: " ;
-    //cin >> x0 >> y0 >> x1 >> y1;
-    x0 = 1; y0 = 1; x1 = 1 ; y1 = 100;
-    cout << "Entre com as coordenadas x2, y2, x3, y3, respectivamente: " ;
-    //cin >> x2 >> y2 >> x3 >> y3;
-    x2 = 100; y2 = 100; x3 = 100; y3 = 1;
 
     init();
     glutDisplayFunc(display);
