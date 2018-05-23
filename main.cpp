6c1ef3a6126512f94cf774a6e809bc1616ce4144
Pedro Gabriel <pee.torres@gmail.com>
2018-05-23 19:33:05 -0300

    Com Baleia e Preenchimento
    8 linhas ( ideal é 6 )

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ using namespace std ;
 /*Variaveis Globais*/
 int COLUNAS = 200.0;
 int LINHAS = 200.0;
-int xi = 0, yi = 2;
 int x0,y0,x1,y1;
+int x2,y2,x3,y3;
+int matRaster[201][201];
+
 
 /*Menu de Funcoes*/
 void unit(int, int, float, float, float, float);
@@ -16,10 +18,58 @@ void display();
 void init();
 void line(int, int, int, int);
 void setPixel(int, int);
+void preenchimentoAlastramento();
+void drawPolygonLine();
+
+void preenchimentoAlastramento(int x, int y){
+    if(matRaster[x-1][y]!=1){
+        unit(x-1,y,0, 0, 1.0, 1.0);
+        matRaster[x-1][y]=1;
+        preenchimentoAlastramento(x-1, y);
+    }
+    if(matRaster[x][y-1]!=1){
+        unit(x,y-1,0, 0, 1.0, 1.0);
+        matRaster[x][y-1]=1;
+        preenchimentoAlastramento(x, y-1);
+    }
+    if(matRaster[x+1][y]!=1){
+        unit(x+1,y,0, 0, 1.0, 1.0);
+        matRaster[x+1][y]=1;
+        preenchimentoAlastramento(x+1, y);
+    }
+    if(matRaster[x][y+1]!=1){
+        unit(x,y+1,0, 0, 1.0, 1.0);
+        matRaster[x][y+1]=1;
+        preenchimentoAlastramento(x, y+1);
+    }
+    else{
+        return;
+    }
+}
+
+void drawBaleia(){
+    x0=50; y0=50;x1=50;y1=75;x2=75;y2=90;x3=110;y3=62;
+    int x4=130; int y4=70;int x5=130;int y5=50;int x6=110;int y6=60;int x7=100;int y7=50;
+    line(x0,y0,x1,y1);
+    line(x1,y1,x2,y2);
+    line(x2,y2,x3,y3);
+    line(x3,y3,x4,y4);
+    line(x4,y4,x5,y5);
+    line(x5,y5,x6,y6);
+    line(x6,y6,x7,y7);
+    line(x7,y7,x0,y0);
+}
+
+void drawPolygonLine(){
+    line(x0,y0,x1,y1);
+    line(x1,y1,x2,y2);
+    line(x2,y2,x3,y3);
+    line(x3,y3,x0,y0);
+}
 
 void setPixel(int x, int y){
+    matRaster[x][y] = 1;
     unit(x, y, 0, 0, 1.0, 1.0);
-    glFlush();
 }
 
 void line(int x0, int y0, int x1, int y1){
@@ -48,18 +98,20 @@ void unit(int x, int y,float r, float g, float b, float alpha){
 
     glColor4f(r,g,b,alpha);
 
-    glBegin(GL_POLYGON);
+    glPointSize(2);
+    glBegin(GL_POINTS);
         glVertex2f(x,y);
-        glVertex2f(x+1,y);
-        glVertex2f(x+1,y+1);
-        glVertex2f(x,y+1);
     glEnd();
+    glFlush();
 }
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
-    line(x0, y0, x1, y1);
-    //glutPostRedisplay();
+    //line(x0, y0, x1, y1);
+    drawBaleia();
+    //drawPolygonLine();
+    preenchimentoAlastramento(60,60);
+
 }
 
 void init(){
@@ -70,6 +122,11 @@ void init(){
     glLoadIdentity();
     glOrtho (0.0 , COLUNAS , 0.0 , LINHAS , -1.0, 200);
     glMatrixMode(GL_MODELVIEW);
+    for(int i=0; i<200; i++){
+        for(int j=0; j<200 ;j++){
+            matRaster[i][j] = 0;
+        }
+    }
 }
 
 int main(int argc, char** argv){
@@ -81,7 +138,11 @@ int main(int argc, char** argv){
     glutCreateWindow("Pratica Rasterizacao");
 
     cout << "Entre com as coordenadas x0, y0, x1, y1, respectivamente: " ;
-    cin >> x0 >> y0 >> x1 >> y1;
+    //cin >> x0 >> y0 >> x1 >> y1;
+    x0 = 1; y0 = 1; x1 = 1 ; y1 = 100;
+    cout << "Entre com as coordenadas x2, y2, x3, y3, respectivamente: " ;
+    //cin >> x2 >> y2 >> x3 >> y3;
+    x2 = 100; y2 = 100; x3 = 100; y3 = 1;
 
     init();
     glutDisplayFunc(display);
